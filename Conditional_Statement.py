376278688d9989de7948485a2d7861e2f6d9dac8
KilHyun-Kim <rlaarlfgus@gmail.com>
2020-08-11 00:05:03 +0900

    Third Conditional Statement.

---
+++ Conditional_Statement.py
@@ -0,0 +1,28 @@
+# 조건문
+
+'''
+if 조건 :
+ 실행문
+'''
+
+# if ~ else
+'''
+if num%2 == 0:
+ print("짝수")
+else :
+ print("홀수")
+'''
+
+# if ~ elif (else if)
+
+'''
+if age <= 7:
+ print("7이하")
+elif age <= 9:
+ print("7초과 9이하")
+elif age >= 20:
+ print("20 이상")
+'''
+
+# if ~ elif ~ else
+
